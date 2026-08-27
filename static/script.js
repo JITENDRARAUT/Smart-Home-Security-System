31e20d08b0bfef0afd9347bc85b66208ea5195de
JITENDRARAUT <jitendraraut903@gmail.com>
2026-08-27 08:26:47 +0545

    Update script.js

--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@ const motionText = document.getElementById("motion");
 const objectTagText = document.getElementById("detected-object-tag");
 const doorText = document.getElementById("door");
 const alarmText = document.getElementById("alarm");
-const alertsList = document.getElementById("alerts");
+const alertsList = document.getElementById("alerts"); 
 const connectionStatus = document.getElementById("connectionStatus");
 const doorIcon = document.getElementById("door-icon");
 
